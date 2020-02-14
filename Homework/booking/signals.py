a911379883849be4bd0a5c0fa17f1362733898fb
RishitAghera <rishit4879@gmail.com>
2020-02-14 19:06:31 +0530

    test2 done

--- Homework/booking/signals.py
+++ Homework/booking/signals.py
@@ -25,7 +25,7 @@ def email_send(sender,instance,created,**kwargs):
 
         cleaner_msg = "You have been Booked for a Date:" + str(instance.date) + SLOT_CHOICE[int(instance.timeslot)][1] + " for cleaning service at " + instance.city.city_name + "\nCustomer name : " + instance.user.first_name + "\nSee Your Orders List :127.0.0.1:8000/bookorder/'"
         print(cleaner_msg)
-        customer_msg = "COngratulations!You have successfully booked a cleaner for date :" + str(instance.date) + " cleaning service at " + instance.city.city_name + "\nCustomer name : " + instance.user.first_name + "\nSee Your Orders List :127.0.0.1:8000/bookorder/'"
+        customer_msg = "Congratulations!You have successfully booked a cleaner for date :" + str(instance.date) + " cleaning service at " + instance.city.city_name + "\nCustomer name : " + instance.user.first_name + "\nSee Your Orders List :127.0.0.1:8000/bookorder/'"
         print(customer_msg)
         cleaner_mail = ('You Booked', cleaner_msg, EMAIL_HOST_USER, [instance.cleaner.user.email])
         customer_mail = ('You Book', customer_msg, EMAIL_HOST_USER, [instance.user.email])
--- Homework/booking/urls.py
+++ Homework/booking/urls.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.http import Http404
 from django.urls import path,include
 from .views import *
 
@@ -6,14 +7,19 @@ from .views import *
 
 app_name='booking'
 
-urlpatterns = [
-    #booking/book/
-    path('book/',BookingView.as_view(),name='book'),
-    path('bookingdetail/<int:pk>/',BookingDetail.as_view(),name='bookinglist'),
-    path('dutydetail/<int:pk>/',Dutydetail.as_view(),name='dutydetail'),
-    path('bookingsave/',BookingSave.as_view(),name='bookingsave'),
-    path('mybookinglist/',BookingList.as_view(),name='mybookinglist'),
-    path('delete/<int:pk>/',DeleteBooking.as_view(),name='deletebooking')
-    # path('postbooking/',PostBooking.as_view(),name='postbooking')
+try:
 
-]
+    urlpatterns = [
+        #booking/book/
+        path('book/',BookingView.as_view(),name='book'),
+        path('bookingdetail/<int:pk>/',BookingDetail.as_view(),name='bookinglist'),
+        path('dutydetail/<int:pk>/',Dutydetail.as_view(),name='dutydetail'),
+        path('bookingsave/',BookingSave.as_view(),name='bookingsave'),
+        path('mybookinglist/',BookingList.as_view(),name='mybookinglist'),
+        path('delete/<int:pk>/',DeleteBooking.as_view(),name='deletebooking')
+        # path('postbooking/',PostBooking.as_view(),name='postbooking')
+
+    ]
+
+except:
+    raise Http404()
