3776ea9ed45a0a6fb84ba96434ff9943e936524a
RishitAghera <rishit4879@gmail.com>
2020-02-17 10:28:41 +0530

    Done

--- Homework/Homework/settings.py
+++ Homework/Homework/settings.py
@@ -135,4 +135,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'rishit4879@gmail.com'
-EMAIL_HOST_PASSWORD = 'sekxkokpxoupiaok'
+# EMAIL_HOST_PASSWORD = 'your password'
